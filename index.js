c1041ac89e9621a0285d5f97036ce484f790c3d0
PK <nozumaz@gmail.com>
2023-03-22 13:01:17 -0400

    adds functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (activity) return `This Saturday, I want to ${activity}!`;
+    return 'This Saturday, I want to roller-skate!';
+}
+
+function mondayWork(activity) {
+    if (activity) return `This Monday, I will ${activity}.`;
+    return 'This Monday, I will go to the office.';
+}
+
+function wrapAdjective(flair = "*"){
+    const innerFunction = function (adj) {
+        return `You are ${flair}${adj}${flair}!`
+    }
+    return innerFunction;
+}
